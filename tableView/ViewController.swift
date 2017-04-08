71d4fee28e5788e0925bbf8b94de3cd91899229e
Dakshesh patel <ddp88@scarletmail.rutgers.edu>
2017-04-08 15:45:41 -0400

    Initial Commit

---
+++ tableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by Dakshesh patel on 4/8/17.
+//  Copyright © 2017 Dakshesh patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
